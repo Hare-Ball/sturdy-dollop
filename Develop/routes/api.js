9c77f8e529ab7650602f59a12f1eafb3d38fa4ee
Hare-Ball <robby.hare@gmail.com>
2021-09-06 16:54:00 -0500

    updates to server.js and starter code for routes in api.js

---
+++ Develop/routes/api.js
@@ -0,0 +1,10 @@
+const router = require("express").Router();
+
+//add more here
+
+router.get();
+
+router.post();
+
+
+module.exports = router;
--- server.js
+++ server.js
@@ -1,17 +1,21 @@
 const express = require("express");
 const mongoose = require("mongoose");
+const routes = require("./routes/api.js")
 
 const PORT = process.env.PORT || 3000;
 
 const app = express();
 
+app.use(logger("dev"));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+app.use(express.static(path.join(_dirname, "public")));
+app.use(routes);
 
-app.use(express.static("public"));
-
-// routes
-app.use(require("./routes/api.js"));
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workouts", {
+    userNewUrlParser: true,
+    useFindAndModify: false,
+});
 
 app.listen(PORT, () => {
   console.log(`App running on port ${PORT}!`);
